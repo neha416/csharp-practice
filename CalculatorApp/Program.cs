9bbca887d2d516ad131947334402e5d8ca56b54d
Neha Kumari <nehaku@kth.se>
2026-04-16 15:13:42 +0200

    Added Calculator and LargestNumber apps

---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,4 @@
+﻿int a = 5;
+int b = 3;
+
+Console.WriteLine("Sum: " + (a + b));
---
+++ LargestNumber/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] numbers = { 3, 7, 2, 9, 5 };
+int max = numbers[0];
+
+foreach (int num in numbers)
+{
+    if (num > max)
+        max = num;
+}
+
+Console.WriteLine("Largest: " + max);
